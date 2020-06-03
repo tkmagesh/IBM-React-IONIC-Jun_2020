6df124eaa15c31f1e0273c48f222758b158622e3
Magesh Kuppan <tkmagesh77@gmail.com>
2020-06-03 15:01:18 +0530

    moving bindActionCreators to state manager

--- 02-state-manager/state-manager.js
+++ 02-state-manager/state-manager.js
@@ -31,6 +31,17 @@ var StateManager = (function(){
         return store;
     }
 
-    return { createStore };
+    function bindActionCreators(actionCreators, dispatch) {
+        var actionDispatchers = {};
+        for (let key in actionCreators) {
+            actionDispatchers[key] = function (...args) {
+                const action = actionCreators[key](...args);
+                dispatch(action);
+            }
+        }
+        return actionDispatchers;
+    }
+
+    return { createStore, bindActionCreators };
 
 })();
